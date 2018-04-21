c2cc37aad93a000a78d272bf699569d297ef6b73
knolle <knolle@creatorcat.de>
2018-04-21 21:02:18 +0200

    Add sample test code: common unit test

---
+++ sampleUnitTest-common/src/main/kotlin/de/creatorcat/samples/Sample.kt
@@ -0,0 +1,3 @@
+package de.creatorcat.samples
+
+fun foo() = "foo"
---
+++ sampleUnitTest-common/src/test/kotlin/de/creatorcat/samples/SampleTest.kt
@@ -0,0 +1,10 @@
+package de.creatorcat.samples
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SampleTest {
+    @Test fun verify_foo() {
+        assertEquals("foo", foo())
+    }
+}
